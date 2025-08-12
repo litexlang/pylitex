4d99d8d90f7dbaca0bf9587af47b672cb0a6de53
kidhero <scanfandprintf@icloud.com>
2025-08-12 10:01:20 +0800

    Add multithreads as option

--- pylitex/__init__.py
+++ pylitex/__init__.py
@@ -1,3 +1,4 @@
 """Python api for Litex core"""
 
-from .pylitex import get_version, get_litex_version, run, run_batch
+from .pylitex import get_version, get_litex_version, run, auto_model_determination, run_batch
+from .enum import RunBatchModel
--- pylitex/pylitex.py
+++ pylitex/pylitex.py
@@ -1,8 +1,11 @@
+import threading
 import multiprocessing as mp
 import subprocess
 import re
 
-__version__ = "0.1.2"
+from .enum import RunBatchModel
+
+__version__ = "0.1.3"
 version_pat = re.compile(r"Litex Kernel: golitex (.*)")
 litex_path = "litex"
 
@@ -40,9 +43,46 @@ def run(code: str) -> str:
         return "Litex command not found. Please ensure Litex is installed and in your PATH."
 
 
-# TODO add options for running code snippets in parallel
-def run_batch(codes: list[str], max_workers: int = 1) -> list[str]:
-    """Run a batch of code snippets in parallel."""
+def run_batch_multithread(codes: list[str]) -> list[str]:
+    """Run a batch of code snippets in parallel using threads."""
+    threads = []
+    for code in codes:
+        thread = threading.Thread(target=run, args=(code,))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+    return [thread.result for thread in threads]
+
+
+def run_batch_multiprocess(codes: list[str], max_workers: int = 1) -> list[str]:
+    """Run a batch of code snippets in parallel using processes."""
     with mp.Pool(processes=max_workers) as pool:
         results = pool.map(run, codes)
     return results
+
+
+# TODO Implement a function to determine the best model to use for running batches
+def auto_model_determination() -> RunBatchModel:
+    """Determine the best model to use for running batches."""
+    return RunBatchModel.MULTIPROCESS  # Placeholder for actual logic
+
+
+def run_batch(
+    codes: list[str], max_workers: int = 1, model: RunBatchModel = RunBatchModel.AUTO
+) -> list[str]:
+    """Run a batch of code snippets in parallel."""
+    if model == RunBatchModel.MULTIPROCESS:
+        return run_batch_multiprocess(codes, max_workers)
+    elif model == RunBatchModel.MULTITHREAD:
+        return run_batch_multithread(codes)
+    elif model == RunBatchModel.AUTO:
+        target_model = auto_model_determination()
+        if target_model == RunBatchModel.MULTIPROCESS:
+            return run_batch_multiprocess(codes, max_workers)
+        elif target_model == RunBatchModel.MULTITHREAD:
+            return run_batch_multithread(codes)
+        else:
+            raise ValueError(f"Unsupported auto-determined model: {target_model}")
+    else:
+        raise ValueError(f"Unsupported RunBatchModel: {model}")
